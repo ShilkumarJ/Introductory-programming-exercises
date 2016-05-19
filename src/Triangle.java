c17b109cccbfd16644347e38e9f8d2b85a88c103
ShilkumarJ <shilkumarjadhav@gmail.com>
2016-05-19 11:53:07 +0530

    Added method to print one asterisk

---
+++ src/Triangle.java
@@ -0,0 +1,11 @@
+
+public class Triangle {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        triangle.printOneAsterisk();
+    }
+
+    private void printOneAsterisk() {
+        System.out.println("*");
+    }
+}
