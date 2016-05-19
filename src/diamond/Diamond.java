c969abec8e9adb1778098c4f5f3d2bfce1907d33
ShilkumarJ <shilkumarjadhav@gmail.com>
2016-05-19 13:36:14 +0530

    Added code to draw diamond with name at centre

--- src/diamond/Diamond.java
+++ src/diamond/Diamond.java
@@ -7,26 +7,39 @@ public class Diamond {
 
     public static void main(String[] args) {
         Diamond diamond = new Diamond(3);
-        diamond.draw();
+        diamond.drawWithName();
+    }
+
+    private void drawWithName() {
+        drawUpperPart();
+        System.out.println("Shilkumar");
+        drawLowerPart();
     }
 
     private void draw() {
         drawUpperPart();
+        drawMiddlePart();
         drawLowerPart();
+    }
+
+    private void drawMiddlePart() {
+        Line line = new Line(2 * (height-1) + 1);
+        line.drawHorizontalLine(0);
+        System.out.println();
     }
 
     private void drawLowerPart() {
         for(int i= this.height-2;i>=0;i--){
             Line line = new Line(2 * i + 1);
-            line.drawHorizontalLine(this.height-i);
+            line.drawHorizontalLine(this.height-i-1);
             System.out.println();
         }
     }
 
     private void drawUpperPart() {
-        for(int i=0;i<this.height;i++){
+        for(int i=0;i<this.height-1;i++){
             Line line = new Line(2 * i + 1);
-            line.drawHorizontalLine(this.height-i);
+            line.drawHorizontalLine(this.height-i-1);
             System.out.println();
         }
     }
